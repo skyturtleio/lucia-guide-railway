06ce3e9ae44ddc751e5c58258dd3fa5aacb6e24e
Leo Mendoza Jr <dev@skyturtle.io>
2023-10-23 10:29:23 -0700

    add pending ui state when logging in

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -3,21 +3,36 @@
 	import type { ActionData } from './$types';
 
 	export let form: ActionData;
+	let submitting = false;
 </script>
 
 <h1>Login</h1>
 
-<form method="POST" use:enhance>
+<form
+	method="POST"
+	use:enhance={() => {
+		submitting = true;
+
+		return async ({ update }) => {
+			await update();
+			submitting = false;
+		};
+	}}
+>
 	<label for="email">
 		<span>Email</span>
-		<input name="email" id="email" type="text" />
+		<input name="email" id="email" type="text" disabled={submitting} />
 	</label>
 	<label for="password">
 		<span>Password</span>
-		<input name="password" id="password" type="password" />
+		<input name="password" id="password" type="password" disabled={submitting} />
 		<button type="submit">Submit</button>
 	</label>
 </form>
+
+{#if submitting}
+	<span>submitting...</span>
+{/if}
 
 {#if form?.message}
 	<p class="error">{form.message}</p>
